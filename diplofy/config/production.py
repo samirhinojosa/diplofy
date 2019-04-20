e45806d865fae1b6dd3b96bddca99508f01c3c0d
Samir Hinojosa <samirhinojosa@gmail.com>
2019-04-20 10:20:20 -0300

    Added database's settings

--- diplofy/config/production.py
+++ diplofy/config/production.py
@@ -1,7 +1,6 @@
 from .base import *
 
 
-
 ALLOALLOWED_HOSTS = ['159.203.41.156']
 
 
@@ -10,11 +9,10 @@ ALLOALLOWED_HOSTS = ['159.203.41.156']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'diplofy_db',
+        'NAME': 'diplofydb',
         'USER': 'diplofy_usr',
-        'PASSWORD': 'diplofy_pwd',
+        'PASSWORD': 'Diplo@PWD#DB2019_',
         'HOST': 'db',                     
         'PORT': '5432', 
     }
 }
-
