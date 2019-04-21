6c3f44ee8cbf07f9a33abac6a3bbd9956f0e6e90
Samir Hinojosa <samirhinojosa@gmail.com>
2019-04-21 14:36:18 -0300

    Updated url for production

--- diplofy/urls.py
+++ diplofy/urls.py
@@ -33,6 +33,6 @@ urlpatterns = [
     path('myadmin/', diplofy_admin_site.urls),
 ]
 
-if settings.DEBUG is True:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG is True:
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
